c81dc64b21f5fd0089cf8fbeccf0ff0f3b3675e6
compkira <105217658+compkira@users.noreply.github.com>
2022-05-13 04:00:36 +0800

    Create ImageNet.py

---
+++ VIT Attention Map/src/main/data/ImageNet.py
@@ -0,0 +1,23 @@
+from torchvision.datasets import ImageNet
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import os
+
+from src.main.config.config_factory import ConfigFactory
+
+
+mc, _ = ConfigFactory()
+image_size = (mc.image_size[1], mc.image_size[2])
+
+transform = transforms.Compose([
+    # transforms.Resize(image_size),
+    transforms.ToTensor()  # convert to tensor
+])
+
+# load data
+download = False if os.path.exists("src/main/data/ImageNet") else True
+# trainset = CIFAR100("src/main/data", train=True, download=download, transform=transform)
+valset = ImageNet("src/main/data", split="val", download=download, transform=transform)
+
+# trainloader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=1)
+valloader = DataLoader(valset, batch_size=128, shuffle=True)
